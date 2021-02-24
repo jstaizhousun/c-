52433de7a171f23e2f71fb40e2ec0ac504db4417
shanshuiyicheng <466278665@qq.com>
2021-02-24 09:43:07 +0800

    添加联系人功能

--- 通讯录管理/源.cpp
+++ 通讯录管理/源.cpp
@@ -32,6 +32,44 @@ struct AddressBooks {
 	//通讯录中当前记录的个数
 	int m_Size;
 };
+//添加联系人
+void addPerson(AddressBooks* abs) {
+	//判断通讯录是否已满
+	if (abs->m_Size == MAX) {
+		cout << "通讯录已满，无法添加" << endl;
+	}
+	else {
+		//添加具体联系人
+
+		//姓名
+		cout << "请输入姓名" << endl;
+		cin >> abs->personArray[abs->m_Size].m_name;
+		//性别
+		cout << "请输入性别，1为男，2为女" << endl;
+		int sex = 0;
+		while (true) {
+			cin >> sex;
+			if (sex == 1 || sex == 2) {
+				abs->personArray[abs->m_Size].m_Sex = sex;
+				break;
+			}
+			else {
+				cout << "输入错误，请重新输入性别，1为男，2为女" << endl;
+
+			}
+		};
+		//年龄
+		cout << "请输入年龄" << endl;
+		int age = 0;
+		cin >> age;
+		abs->personArray[abs->m_Size].m_Age = age;
+		//电话
+		cout << "请输入电话" << endl;
+		string phone ="0";
+		cin >> phone;
+		abs->personArray[abs->m_Size].m_phone = phone;
+	}
+};
 int main() {
 	//创建通讯录结构体变量
 	AddressBooks abs;
@@ -47,6 +85,7 @@ int main() {
 		switch (select) {
 			//添加联系人
 		case 1:
+			addPerson(&abs);
 			break;
 			//显示联系人
 		case 2:
